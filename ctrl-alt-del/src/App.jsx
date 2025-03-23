cdce91f3dc3b237d0db355648789bfab149f6920
Prachi <prachishaw04@gmail.com>
2025-03-24 01:30:38 +0530

    Initial commit

--- ctrl-alt-del/src/App.jsx
+++ ctrl-alt-del/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Home from './pages/Home'
 import './styles/home.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
 
--- ctrl-alt-del/src/components/Navbar.jsx
+++ ctrl-alt-del/src/components/Navbar.jsx
@@ -1,14 +1,45 @@
-import "../styles/navbar.css"; // Importing the CSS file
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import logo from "../assets/logo.webp";
+import "../styles/navbar.css";
 
-export default function Navbar() {
+const MentalHealthNavbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Our Services</a></li>
-        <li><a href="#">Contact Us</a></li>
-      </ul>
-    </nav>
+    <Navbar expand="lg" bg="light" variant="light" className="px-3">
+      <Container>
+        {/* Logo */}
+        <Navbar.Brand href="#home" className="brand-container">
+          <div className="logo-wrapper">
+            <img src={logo} alt="Logo" className="app-logo" />
+            <span className="app-name">Ctrl+Alt+Del</span>
+          </div>
+        </Navbar.Brand>
+
+
+        {/* Toggle Button */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {/* Main Links */}
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About Us</Nav.Link>
+            <Nav.Link href="#contact">Contact Us</Nav.Link>
+            <Nav.Link href="#login">Login / Signup</Nav.Link>
+
+            {/* Additional Links in Dropdown */}
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#journal">Write Your Journal 📝 </NavDropdown.Item>
+              <NavDropdown.Item href="#blogs">Read Blogs 📰 </NavDropdown.Item>
+              <NavDropdown.Item href="#track mood">Track your mood 🧐 </NavDropdown.Item>
+              <NavDropdown.Item href="#analysis">Analyze your journey 📊 </NavDropdown.Item>
+              <NavDropdown.Item href="#rewards">Badges earned 🏆</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-}
+};
+
+export default MentalHealthNavbar;
+

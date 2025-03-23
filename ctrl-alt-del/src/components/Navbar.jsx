54860408e9c80680cd8e0f72b7ddff6d40508f00
Prachi <prachishaw04@gmail.com>
2025-03-23 23:33:42 +0530

    navbar added

--- ctrl-alt-del/src/components/Navbar.jsx
+++ ctrl-alt-del/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-
+import "../styles/navbar.css"; // Importing the CSS file
 
 export default function Navbar() {
-
-    return (
-      <>
-        
-
-      </>
-    )
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Our Services</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
 }
-  

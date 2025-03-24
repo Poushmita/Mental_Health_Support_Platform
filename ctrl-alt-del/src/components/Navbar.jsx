0f69ac77e2e69fbea3a2bf80a34df861a1c86b96
Poushmita <poushmitap@gmail.com>
2025-03-24 20:25:16 +0530

    changed navbar styling and image slider

--- ctrl-alt-del/src/components/Navbar.jsx
+++ ctrl-alt-del/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
-import logo from "../assets/logo.webp";
+import logo from "../assets/logo3.jpg";
 import "../styles/navbar.css";
 
 const MentalHealthNavbar = () => {
   return (
-    <Navbar expand="lg" bg="light" variant="light" className="px-3">
+    <Navbar expand="lg"  className="px-3 navbar" style={{backgroundColor: '#84a160'}}>
       <Container>
         {/* Logo */}
         <Navbar.Brand href="#home" className="brand-container">
           <div className="logo-wrapper">
             <img src={logo} alt="Logo" className="app-logo" />
-            <span className="app-name">Ctrl+Alt+Del</span>
+            {/* <span className="app-name">Ctrl+Alt+Del</span> */}
           </div>
         </Navbar.Brand>
 
@@ -19,7 +19,7 @@ const MentalHealthNavbar = () => {
         {/* Toggle Button */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="ms-auto font-pacifico font-bold text-black">
             {/* Main Links */}
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About Us</Nav.Link>
--- ctrl-alt-del/src/pages/Home.jsx
+++ ctrl-alt-del/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import slide1 from "../assets/slide1.png";
-import slide2 from "../assets/slide2.png";
+import slide2 from "../assets/slide2.jpg";
 import slide3 from "../assets/slide3.png";
 import slide4 from "../assets/slide4.png";
 import slide5 from "../assets/slide5.png";
@@ -14,47 +14,49 @@ export default function Home() {
     return ( 
       <>
         <Navbar />
-        <section className="sec2">
-          <div id="carouselExampleIndicators" className='carousel slide'>
-            <div className="carousel-indicators">
-              <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='0' className='active' aria-current='true' aria-label='slide 1'></button>
-              <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='1' aria-label='Slide 2'></button>
-              <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='2' aria-label='Slide 3'></button>
-              <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='3' aria-label='Slide 4'></button>
-              <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='4' aria-label='Slide 5'></button>
-              <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='5' aria-label='Slide 6'></button>
+        <section className="sec1">
+            <div id="carouselExampleAutoplaying" class="carousel carousel-dark slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src={slide1} class="d-block w-100" alt="Slide 1" />
+                </div>
+                <div class="carousel-item">
+                  <img src={slide2} class="d-block w-100" alt="Slide 2" />
+                </div>
+                <div class="carousel-item">
+                  <img src={slide3} class="d-block w-100" alt="Slide 3" />
+                </div>
+                <div class="carousel-item">
+                  <img src={slide4} class="d-block w-100" alt="Slide 4" />
+                </div>
+                <div class="carousel-item">
+                  <img src={slide5} class="d-block w-100" alt="Slide 5" />
+                </div>
+                <div class="carousel-item">
+                  <img src={slide6} class="d-block w-100" alt="Slide 6" />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
             </div>
-
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img src={slide1} className="d-block w-100" alt="Slide 1"/>
-              </div>
-              <div className="carousel-item">
-                <img src={slide2} className="d-block w-100" alt="Slide 2"/>
-              </div>
-              <div className="carousel-item">
-                <img src={slide3} className="d-block w-100" alt="Slise 3"/>
-              </div>
-              <div className="carousel-item">
-                <img src={slide4} className="d-block w-100" alt="Slise 4"/>
-              </div>
-              <div className="carousel-item">
-                <img src={slide5} className="d-block w-100" alt="Slise 5"/>
-              </div>
-              <div className="carousel-item">
-                <img src={slide6} className="d-block w-100" alt="Slise 6"/>
-              </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div> 
         </section>
+
+
+        <section className='sec2'>
+          <h1>section2</h1>
+        </section>
+
+
+        <section className='sec3'>
+          <h1>section3</h1>
+        </section>
+
 
         <Footer />
       </>

7b1ebcebaec0c91c49d1cb9e0fd9851e94dcf910
Kusum Kumari <kusumraigrd@gmail.com>
2025-03-24 15:53:44 +0530

    Added new feature to Home page

--- ctrl-alt-del/src/pages/Home.jsx
+++ ctrl-alt-del/src/pages/Home.jsx
@@ -1,4 +1,10 @@
-import home1 from '../assets/home1.jpg';
+import React from "react";
+import slide1 from "../assets/slide1.png";
+import slide2 from "../assets/slide2.png";  
+import slide3 from "../assets/slide3.png";  
+import slide4 from "../assets/slide4.png";
+import slide5 from "../assets/slide5.png";
+import slide6 from "../assets/slide6.png";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
@@ -6,28 +12,48 @@ export default function Home() {
     return ( 
       <>
         <Navbar />
-        <section
-          className="sec1 grid place-items-center bg-no-repeat bg-cover bg-center w-full min-h-screen"
-          style={{backgroundImage: `url(${home1})`}}
-        >
-          <h1 className="text-3xl font-serif text-black">
-            Section 1
-          </h1>
-        </section>
-
-        <section className="sec2">
-          <div>
-              <h1 className="m-2">section2</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ullam perferendis, quaerat quasi quos expedita!</p>
-              <button class="btn btn-success">Bootstrap Button</button>
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Click Me
-              </button>
-
+        <section>
+          <div id="carouselExampleIndicators" className="carousel slide">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
           </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={slide1} className="d-block w-100" alt="Slide 1"/>
+            </div>
+            <div className="carousel-item">
+              <img src={slide2} className="d-block w-100" alt="Slide 2"/>
+            </div>
+            <div className="carousel-item">
+              <img src={slide3} className="d-block w-100" alt="Slise 3"/>
+            </div>
+            <div className="carousel-item">
+              <img src={slide4} className="d-block w-100" alt="Slise 4"/>
+            </div>
+            <div className="carousel-item">
+              <img src={slide5} className="d-block w-100" alt="Slise 5"/>
+            </div>
+            <div className="carousel-item">
+              <img src={slide6} className="d-block w-100" alt="Slise 6"/>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
         </section>
 
       </>
-    )
-  }
+    );
+  };
   
